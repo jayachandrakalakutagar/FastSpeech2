5449c4111599ba6e789a3d9d355268ab10aa0eec
jayachandrakalakutagar <96902361+jayachandrakalakutagar@users.noreply.github.com>
2022-06-13 11:44:57 +0530

    Update vctk.py

--- preprocessor/vctk.py
+++ preprocessor/vctk.py
@@ -13,23 +13,23 @@ def prepare_align(config):
         if file_name[-4:]!=".txt":
             continue
         base_name=file_name[:-4]
-        text_path=os.path.join(in_dir,"txt","p294","{}.txt".format(base_name))
-        wav_path=os.path.join(in_dir,"wav48","p294","{}.wav".format(base_name))
+        text_path=os.path.join(in_dir,"txt","p300","{}.txt".format(base_name))
+        wav_path=os.path.join(in_dir,"wav48","p300{}.wav".format(base_name))
 
         with open(text_path) as f:
                 text = f.readline().strip("\n")
 
-        os.makedirs(os.path.join(out_dir, "p294"), exist_ok=True)
+        os.makedirs(os.path.join(out_dir, "p300 exist_ok=True)
         wav, _ = librosa.load(wav_path, sampling_rate)
         wav = wav / max(abs(wav)) * 32768.0
 
         wavfile.write(
-            os.path.join(out_dir, "p294", "{}.wav".format(base_name)),
+            os.path.join(out_dir, "p300"{}.wav".format(base_name)),
             sampling_rate,
             wav.astype(np.int16),
         )
         with open(
-            os.path.join(out_dir, "p294", "{}.lab".format(base_name)),
+            os.path.join(out_dir, "p300{}.lab".format(base_name)),
             "w",
          ) as f1:
             f1.write(text)
