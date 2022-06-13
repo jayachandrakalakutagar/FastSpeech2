a3acd4fbfd1447701d741d56ce628c34751027a6
jayachandrakalakutagar <96902361+jayachandrakalakutagar@users.noreply.github.com>
2022-06-13 13:16:40 +0530

    Update vctk.py

--- preprocessor/vctk.py
+++ preprocessor/vctk.py
@@ -9,27 +9,27 @@ def prepare_align(config):
     out_dir = config["path"]["raw_path"]
     sampling_rate = config["preprocessing"]["audio"]["sampling_rate"]
     max_wav_value = config["preprocessing"]["audio"]["max_wav_value"]
-    for file_name in (os.listdir(os.path.join(in_dir,"txt","p294"))):
+    for file_name in (os.listdir(os.path.join(in_dir,"txt","p300"))):
         if file_name[-4:]!=".txt":
             continue
         base_name=file_name[:-4]
         text_path=os.path.join(in_dir,"txt","p300","{}.txt".format(base_name))
-        wav_path=os.path.join(in_dir,"wav48","p300{}.wav".format(base_name))
+        wav_path=os.path.join(in_dir,"wav48","p300","{}.wav".format(base_name))
 
         with open(text_path) as f:
                 text = f.readline().strip("\n")
 
-        os.makedirs(os.path.join(out_dir, "p300 exist_ok=True)
+        os.makedirs(os.path.join(out_dir, "p300", exist_ok=True)
         wav, _ = librosa.load(wav_path, sampling_rate)
         wav = wav / max(abs(wav)) * 32768.0
 
         wavfile.write(
-            os.path.join(out_dir, "p300"{}.wav".format(base_name)),
+            os.path.join(out_dir, "p300","{}.wav".format(base_name)),
             sampling_rate,
             wav.astype(np.int16),
         )
         with open(
-            os.path.join(out_dir, "p300{}.lab".format(base_name)),
+            os.path.join(out_dir, "p300","{}.lab".format(base_name)),
             "w",
          ) as f1:
             f1.write(text)
